8524bcdbd26e8d71c6b4826feee414096fc63d82
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-09-21 18:44:49 +0000

    Add a new file book.rb, update file topic.rb

--- no_starch_press/lib/no_starch_press/topic.rb
+++ no_starch_press/lib/no_starch_press/topic.rb
@@ -1,7 +1,17 @@
 class NoStarchPress::Topic
+  attr_accessor :name, :url, :books 
+  @@all = []
+  
+  def initialize(name = nil, url = nil)
+    @name - name
+    @url = url
+    @books = []
+    @@all << self
+  end 
   
   def self.all
-    puts "Welcome to No Starch Press!" 
+    @@all
+    
     puts "1. Ardruino"
     puts "2. Art & Design"
     puts "3. General Computing"
